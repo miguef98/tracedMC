0b3b738f7417ea5b8feea3e8076e335312650755
Migue Fainstein <miguelon.f98@gmail.com>
2024-04-19 22:09:45 -0300

    andanding

--- mc.py
+++ mc.py
@@ -3,6 +3,7 @@ from dudf.model import SIREN
 from dudf.evaluate import evaluate
 from scipy.optimize import minimize
 import torch
+import networkx as nx
 
 model = SIREN(
             n_in_features= 3,
@@ -12,16 +13,17 @@ model = SIREN(
             ww=None,
             activation='sine'
 )
-model.load_state_dict( torch.load('spot_smooth/model_best.pth'))
+model.load_state_dict( torch.load('models/spot_smooth/model_best.pth'))
 device_torch = torch.device(0)
 model.to(device_torch)
 
 def df( x ):
     # spot dudf
     return evaluate( model, x, device=device_torch)
+    
     # esfera
     return np.linalg.norm( x, axis=1 )[...,None] - 0.5
-    
+
     # spot numerico
     return spotScene.compute_distance( o3c.Tensor(x, dtype=o3c.float32) ).numpy()[...,None]
 
@@ -35,8 +37,11 @@ def g_df( x ):
     pred_normals = eigenvectors[..., 2].numpy()
 
     return pred_normals
+
     # esfera
     return x / np.linalg.norm( x, axis=1 )[..., None] * np.random.choice( [-1,1], (x.shape[0],1))
+
+
 
     # spot numerico
     EPS = 0.001
@@ -47,18 +52,19 @@ def g_df( x ):
 
     return gradient / np.linalg.norm( gradient, axis=1 )[..., None]
 
-def ransac( A, b ):
+def ransac( X, A, b ):
     if A.shape[0] == 3:
-        return leastSquares(A,b)
+        return leastSquares(X, A,b)
     else:
         x_best = None
         error_best = np.inf
         for i in range(20):
             subsetIdx = np.random.choice(A.shape[0], np.random.randint(3, A.shape[0]))
             A_bar = A[ subsetIdx , : ]
+            X_bar = X[ subsetIdx , : ]
             b_bar = b[ subsetIdx ]
 
-            x_bar = leastSquares( A_bar, b_bar )
+            x_bar = leastSquares( X_bar, A_bar, b_bar )
             error_bar = np.linalg.norm( A_bar @ x_bar - b_bar )
 
             if error_best > error_bar:
@@ -67,26 +73,56 @@ def ransac( A, b ):
 
         return x_best            
 
-def leastSquares( A, b, threshold=0.1 ):
-    _, R = np.linalg.qr( np.concatenate([A,b[...,None]], axis = 1), mode='reduced' )
-    A_hat = R[:3, :3]
-    b_hat = R[:3, 3]
-
-    U,S,V = np.linalg.svd( A_hat, full_matrices=True )
-
-    S_inv = np.zeros_like( S )
-    np.divide( np.ones_like(S), S, where=S > threshold, out=S_inv )
-
-    A_hat_pseudoinv = V.T @ np.diag( S_inv ) @ U.T
-
-    return A_hat_pseudoinv @ b_hat
-
-def dualContour( A, b, threshold ):
-    # paper
-    return leastSquares( A, b, threshold=threshold )
+def leastSquares( X, A, b, threshold=0.1 ):
+    U,S,V_t = np.linalg.svd( A )
+    reliableSVs = S > threshold
+    numberSVs = np.sum(reliableSVs)
+    S_inv = np.where( reliableSVs, np.divide( np.ones_like(S), S) , np.zeros_like(S) )
     
+    def solParticular():
+        if A.shape[0] > A.shape[1]:
+            A_pseudoinv = V_t.T @ np.block( [ np.diag( S_inv ), np.zeros( (S_inv.shape[0], U.T.shape[0] - S_inv.shape[0])) ] ) @ U.T
+        elif A.shape[0] < A.shape[1]:
+            A_pseudoinv = V_t.T @ np.vstack( [ np.diag( S_inv ), np.zeros( (V_t.shape[0] - S_inv.shape[0], S_inv.shape[0])) ] ) @ U.T
+        else:
+            A_pseudoinv = V_t.T @ np.diag( S_inv ) @ U.T
+
+        return A_pseudoinv @ b
+
+    if numberSVs == 0:
+        return np.mean(X, axis=0)
+
+    elif numberSVs == 1:
+        c = solParticular()
+        v1 = V_t[1,:]
+        v2 = V_t[2,:]
+        x = np.mean(X, axis=0)
+
+        alpha1 = (x - c) @ v1
+        alpha2 = (x - c) @ v2
+
+        return c + alpha1 * v1 + alpha2 * v2
+
+    elif numberSVs == 2:
+        # solGeneral = solParticular() + alpha * V_t[2,:]
+        c = solParticular()
+        v = V_t[2,:]
+        x = np.mean(X, axis=0)
+        return c + ((v @ (x - c)) / (v @ v) ) * v
+
+    else:
+        return solParticular()
+
+def dualContour( X, A, b, threshold ):
+    # paper
+    return leastSquares( X, A, b, threshold=threshold )
+
     # paper + ransac
-    return ransac( A, b )
+    return ransac(X, A, b )
+
+    #center of mass
+    return np.mean(X, axis=0)
+
 
     # recta plano
     planeParams = leastSquares( np.hstack([X, np.ones((X.shape[0],1)) ]), np.zeros(X.shape[0]) )
@@ -127,7 +163,6 @@ def dualContour( A, b, threshold ):
 
     return leastSquares( A_lst, b_lst )
 
-
     # double energy
     lambda1 = 0 #0.01
     lambda2 = 1
@@ -137,14 +172,10 @@ def dualContour( A, b, threshold ):
 
     A = np.vstack([ np.sqrt(lambda1) * A, np.sqrt(lambda2) * A2 ])
     b = np.vstack([ np.sqrt(lambda1) * b[...,None], np.sqrt(lambda2) * b2[...,None] ]).flatten()
-
-    #center of mass
-    return np.sum(X, axis=0) / len(X)
     
     # bounded optimization
     E =  lambda x: (A @ x - b).T @ (A @ x - b)
     return minimize( E, x0, bounds=bounds ).x
-
 
     # numpy
     x,_,_,sv = np.linalg.lstsq( A, b, rcond=None )
@@ -191,7 +222,7 @@ def updateDict( hitPositions, hitNormals, rayIndexs, N, axis, edgeVertexs, edgeN
                 if tuple(cubeIndex) in cubeData:
                     if edge in cubeData[tuple(cubeIndex)]['edgeVertexIndexs']:
                         raise Exception('ups')
-                        cubeData[tuple(cubeIndex)]['edgeVertexIndexs'][edge].append(edgeVertexIndex)    
+                        cubeData[tuple(cubeIndex)]['edgeVertexIndexs'][edge].append(edgeVertexIndex)
                     else:
                         cubeData[tuple(cubeIndex)]['edgeVertexIndexs'][edge] = [ edgeVertexIndex ]
                 else:
@@ -241,7 +272,7 @@ def rayMarch( rayPositions, axis, df, N, outDict, edgeVertexs, edgeNormals, poly
                 polygons
             )
             
-            rayPositions[surfaceHitRays, axis] =  (np.floor( (surfaceHitPosition[:, axis] + 1) * ((N-1)/2) ) + 1 + 2 * surfaceThresh) * (2/(N-1)) - 1
+            rayPositions[surfaceHitRays, axis] =  (np.floor( (surfaceHitPosition[:, axis] + 1) * ((N-1)/2) ) + 1 + np.random.uniform(0, 2/(N-1))) * (2/(N-1)) - 1
 
 
 def cubeVertexs( N, df, surfaceThresh ):
@@ -255,18 +286,34 @@ def cubeVertexs( N, df, surfaceThresh ):
 
     return cubeData, np.array(edgeVertexs),np.array(edgeNormals), polygons
 
-def cubeCenterVertex( cubeData, edgeVertexs, edgeNormals, N, threshold ):
+def cubeCenterVertex( cubeData, edgeVertexs, edgeNormals, N, threshold, alpha=6 ):
+    global corners
     for cubeIdx, cubeInfo in cubeData.items():
-        X = np.mean( [ edgeVertexs[ idxPerEdge ] for idxPerEdge in cubeInfo['edgeVertexIndexs'].values() ], axis=0)
-        A = np.mean( [ edgeNormals[ idxPerEdge ] for idxPerEdge in cubeInfo['edgeVertexIndexs'].values() ], axis=0)
+        X = np.array( [ np.mean(edgeVertexs[ idxPerEdge ], axis=0) for idxPerEdge in cubeInfo['edgeVertexIndexs'].values() ])
+        A = np.array( [ np.mean(edgeNormals[ idxPerEdge ], axis=0) for idxPerEdge in cubeInfo['edgeVertexIndexs'].values() ])
 
-        if len(A) == 1:
-            cubeData[cubeIdx]['vertex'] = X.flatten()
-        #elif len(A) == 2:
-        #    cubeData[cubeIdx]['vertex'] = (X.sum(0) / 2).flatten()
+        G = X @ X.T
+        P = -2 * G + np.outer( np.diag(G), np.ones(X.shape[0]) ) + np.outer( np.ones(X.shape[0]), np.diag(G) )
+
+        X_new = []
+        A_new = []
+        for components in nx.connected_components( nx.from_numpy_array( P < (2/(alpha*(N-1)) ) ** 2 )):
+            X_new.append( np.mean( X[np.array(list(components)) ], axis=0 ))
+            A_new.append( np.mean( A[np.array(list(components)) ], axis=0 ))
+
+        X_new = np.array(X_new)
+        A_new = np.array(A_new)
+
+        cubeData[cubeIdx]['X'] = X_new
+        cubeData[cubeIdx]['A'] = A_new
+
+        if len(A_new) == 1:
+            cubeData[cubeIdx]['vertex'] = X_new.flatten()
         else:
-            b = np.sum( A * X, axis=1).flatten()
-            dual = dualContour( A, b, threshold=threshold )
+            b = np.sum( A_new * X_new, axis=1).flatten()
+            dual = dualContour( X_new, A_new, b, threshold=threshold )
+
+            #dual = dualContour( A, b, threshold=threshold )
             #bounds = [ (-1 + i*2/(N-1), -1 + (i+1)*2/(N-1)) for i in cubeIdx ]
             #dual = dualContour( A, b, np.sum(X, axis=0) / X.shape[0], bounds )
 
